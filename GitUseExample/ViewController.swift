f2321ed09fa13309c652105aa41c520ba5147195
John Allen <johnallen@Johns-MacBook-Pro-2.local>
2018-04-29 14:51:39 -0400

    another one

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -12,13 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        // Do any additional setup after loading the view, typically from a nib.
+        print("hello")
     }
 
-    @IBAction func buttonClicked(_ sender: UIButton) {
-        print("This is a Git tutorial")
-    }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
