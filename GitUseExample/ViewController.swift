5521e8348752da14eb67618b8cb181b09d0b3e2a
John Allen <johnallen@johns-macbook-pro-2.local>
2018-04-29 15:33:33 -0400

    first online push!

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print("hello")
+        view.backgroundColor = UIColor.blue
+        
     }
 
     override func didReceiveMemoryWarning() {
