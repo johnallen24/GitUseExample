99a920a8c27dbf8394b8dbe52e98cc659d0f8e90
John Allen <johnallen@Johns-MacBook-Pro-2.local>
2018-04-29 15:40:16 -0400

    another one baby

--- GitUseExample/ViewController.swift
+++ GitUseExample/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         print("hello")
         view.backgroundColor = UIColor.blue
+        view.backgroundColor = UIColor.purple
+        
         
     }
 
